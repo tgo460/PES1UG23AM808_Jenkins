8db1945a7ccb3199ae0a30c08f86c0447a6ae801
tgo460 <131740706+tgo460@users.noreply.github.com>
2025-03-16 21:33:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Anupriya add the Github Webhooks  \n";
   return 0;
 }
