b5323c7bc08b445848da3887db8526bd75df731c
tgo460 <131740706+tgo460@users.noreply.github.com>
2025-03-16 19:14:23 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "Github add Webhooks  \n";
+  return 0;
+}
